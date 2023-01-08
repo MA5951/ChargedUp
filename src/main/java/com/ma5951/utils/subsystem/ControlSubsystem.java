75feb037b2df654fb2bf1726997352361df0f4ed
EilonK05 <eilonlon.klein@gmail.com>
2023-01-08 22:06:57 +0200

    inital commit

---
+++ src/main/java/com/ma5951/utils/subsystem/ControlSubsystem.java
@@ -0,0 +1,6 @@
+package com.ma5951.utils.subsystem;
+
+public interface ControlSubsystem extends MotorSubsystem {
+
+    public double getMeasurement();
+}
---
+++ src/main/java/com/ma5951/utils/subsystem/SensorSubsystem.java
@@ -0,0 +1,7 @@
+package com.ma5951.utils.subsystem;
+
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+public interface SensorSubsystem extends Subsystem {
+    public boolean canMove();
+}
