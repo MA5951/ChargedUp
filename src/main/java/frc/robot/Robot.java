dc66d0ba9d5f80a0983a6b53ec424a86ae01a11b
“yoavkaplan123git config --global user.email “yoav110702@gmail.com <yoav110702@gmail.com>
2023-02-05 13:01:18 +0200

    fixed warning

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,10 +9,8 @@ package frc.robot;
 // import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.commands.DriveSwerveCommand;
 import frc.robot.subsystems.swerve.SwerveDrivetrainSubsystem;
 
@@ -26,8 +24,6 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
-  private final static String COMM_STRING = "commands";
-
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
