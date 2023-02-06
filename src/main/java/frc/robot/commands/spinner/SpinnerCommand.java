1743b460f0b90f258cac686107759a92fad7a170
EilonK05 <eilonlon.klein@gmail.com>
2023-02-06 20:53:45 +0200

    Squashed commit of the following:
    commit 82f50e5bf0d843e2b5e3e68554f7bf85a88d1d27
    Author: noya-isaiah <noya2110@gmail.com>
    Date:   Mon Feb 6 20:48:19 2023 +0200
    
        done TODO and little changes
    
    commit 1c349afcac0a5a102fbfb53412ded54a6354ab1c
    Author: EilonK05 <eilonlon.klein@gmail.com>
    Date:   Sat Feb 4 17:09:24 2023 +0200
    
        after CR
    
    commit 7ac9a70a55221bbe2610b8e2b0e50bfbc350d2f2
    Author: Asaf Meizner <75739196+PopiChan@users.noreply.github.com>
    Date:   Tue Jan 31 18:29:41 2023 +0200
    
        Spinner code
    
        Started work on Spinner code and finished skeleton

---
+++ src/main/java/frc/robot/commands/spinner/SpinnerCommand.java
@@ -0,0 +1,60 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+package frc.robot.commands.spinner;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Spinner.SpinnerSubsystem;
+
+public class SpinnerCommand extends CommandBase {
+  /** Creates a new SpinnerCommand. */
+  private SpinnerSubsystem spinnerSubsystem;
+
+  // create a boolean variable to store if the reverse spin action has been completed and when it is completed set it to true
+  private boolean reverseSpinComplete;
+  private boolean reverseSpin;
+  private double startEncoder;
+
+  public SpinnerCommand() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    spinnerSubsystem = SpinnerSubsystem.getInstance();
+
+    addRequirements(spinnerSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    // set reverse spin to false
+    reverseSpin = false;
+    reverseSpinComplete = false;
+    startEncoder = spinnerSubsystem.getEncoder();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (!spinnerSubsystem.getIR()) {
+      spinnerSubsystem.setVelocity(-0.5);
+    } 
+    
+    else if (!reverseSpin) {
+      spinnerSubsystem.setVelocity(0.5);
+      if (spinnerSubsystem.getEncoder() - startEncoder >= 42) {
+        reverseSpin = true;
+        reverseSpinComplete = true;
+      }
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override 
+  public void end(boolean interrupted) {
+    spinnerSubsystem.setVoltage(0);
+  }
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return reverseSpinComplete;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Spinner/SpinnerSubsystem.java
@@ -0,0 +1,62 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.Spinner;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SpinnerSubsystem extends SubsystemBase {
+  // create subsystem
+  private static SpinnerSubsystem spinnerSubsystem;
+  //create spinner motor
+  private CANSparkMax spinnerMotor;
+  //create spinner ir sensor (digital input)
+  private DigitalInput spinnerIR;
+
+  //create spinner motor and ir sensor
+  public SpinnerSubsystem() {
+    //initialize spinner motor
+    spinnerMotor = new CANSparkMax(0, MotorType.kBrushless);
+    //initialize spinner ir sensor
+    spinnerIR = new DigitalInput(0);
+  }
+
+  // get sensor value
+  public boolean getIR() {
+    return spinnerIR.get();
+  }
+
+  // get motor encoder value
+  public double getEncoder() {
+    return spinnerMotor.getEncoder().getPosition();
+  }
+
+  // if sensor is true, spin motor in one direction (clockwise) if false, spin motor in other direction (counter clockwise) for 1 spin (42 motor ticks)
+  public void setVoltage(double voltage) {
+    spinnerMotor.setVoltage(voltage);
+  }
+
+  public void setVelocity(double velocity){
+    spinnerMotor.set(velocity);
+  }
+
+  //TODO: add setPower function to set the motor via precentage input
+
+  // create subsystem method
+  public static SpinnerSubsystem getInstance() {
+    if (spinnerSubsystem == null) {
+      spinnerSubsystem = new SpinnerSubsystem();
+    }
+    return spinnerSubsystem;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
