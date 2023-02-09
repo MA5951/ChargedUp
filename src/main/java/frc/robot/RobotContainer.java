3dd5e33539b56e1168578c96ea8ff11c1fadf0ea
“yoavkaplan123git config --global user.email “yoav110702@gmail.com <yoav110702@gmail.com>
2023-02-09 13:13:18 +0200

    fix fixOdometry

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -95,6 +95,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return SwerveDrivetrainSubsystem.getInstance().getAutonomousPathCommand("try", true);
+    return null;
   }
 }
